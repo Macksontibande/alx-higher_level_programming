3c2cb622351408e04807e09092b89b8c784ba306
Macksontibande <Macksontibade@gmail.com>
2023-04-29 08:35:05 -0700

    0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -10,7 +10,7 @@ def lookup(obj):
     Args:
         obj: initial object
         
-    Returns: a list of available attributes and
-        methods of an object
+    Returns: 
+        list:  list of available attributes and methods of an object
     """
     return dir(obj)
