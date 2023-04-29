8d53a2270625be13cd463ab2983d82e822e6ffe0
Macksontibande <Macksontibade@gmail.com>
2023-04-29 08:12:24 -0700

    3. Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Matias López - <3959@holbertonschool.com>
+"""
+    Kind of a kind
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check instance of object and return true"""
+
+    return (isinstance(obj, a_class))
