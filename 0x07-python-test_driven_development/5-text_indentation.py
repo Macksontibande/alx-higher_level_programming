7801b8281f870aa6349b617a1a474fb9f7b249bd
Macksontibande <Macksontibade@gmail.com>
2023-05-01 00:13:35 -0700

    3. Print square

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    Module to print text with 2 new lines after characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+        Prints a text with 2 new lines after charecter.
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    split_text_list = list()
+    new_text = list()
+    letters_list = list()
+
+    for character in text:
+        letters_list.append(character)
+        if character in ['.', '?', ':']:
+            split_text_list.append(''.join(letters_list))
+            letters_list.clear()
+
+    if letters_list:
+        split_text_list.append("".join(letters_list))
+    letters_list.clear()
+
+    for sentence in split_text_list:
+        new_text.append(sentence.strip(' '))
+    split_text_list.clear()
+
+    for character in new_text[-1]:
+        if character in ['.', '?', ':']:
+            print("\n\n".join(new_text))
+            print()
+            return
+
+    print("\n\n".join(new_text), end='')
