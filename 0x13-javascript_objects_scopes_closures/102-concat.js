cfe6439deb664a332b3dc433934ee16cd3106692
Macksontibande <Macksontibade@gmail.com>
2023-06-16 13:16:20 +0000

    13. Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,5 +4,4 @@ const fs = require('fs');
 const argv = process.argv;
 const file1 = fs.readFileSync(argv[2], 'utf-8').toString();
 const file2 = fs.readFileSync(argv[3], 'utf-8').toString();
-
 fs.writeFileSync(argv[4], file1 + file2);
