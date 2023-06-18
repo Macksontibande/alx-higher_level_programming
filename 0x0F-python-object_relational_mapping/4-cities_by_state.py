34f6058367d5a1eebcb4be4c83478a77f4f68b9c
Macksontibande <Macksontibade@gmail.com>
2023-06-18 15:38:37 -0700

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A script to lists all states with a name starting with N (upper N)
+A script that lists all cities from the database hbtn_0e_4_usa
 """
 import MySQLdb
 from sys import argv
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0] == 'N':
-            print(row)
+        print(row)
     cur.close()
     db.close()
