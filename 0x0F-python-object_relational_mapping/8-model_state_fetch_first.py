17155b9d3a9f686fceb880bfa2190aee4d09e66e
Macksontibande <Macksontibade@gmail.com>
2023-06-23 19:36:40 -0700

    8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""
-Prints the first State object from the database hbtn_0e_6_usa.
+"""Prints the first State object from the database hbtn_0e_6_usa.
+
+Usage: ./8-model_state_fetch_first.py <mysql username>
+                                      <mysql password>
+                                      <database name>
+
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import State
 import sys
 
+# Check if we are running this script at top level.
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
