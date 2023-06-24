ac76dba40be833219948d0ebe022849158e1552d
Macksontibande <Macksontibade@gmail.com>
2023-06-23 21:13:20 -0700

    100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,10 +10,10 @@ from relationship_state import Base, State
 from relationship_city import City
 from sys import argv
 
-
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1]
-        ,argv[2],argv[3]))
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
