b9f08eb6d75cc8f754cb94495a24b26baed909ff
Macksontibande <Macksontibade@gmail.com>
2023-07-14 17:08:03 -0700

    6 & 7. Error code #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script that takes in a URL and an email address,a POST"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {
+        "email": email
+    }
+    response = requests.post(url, data=payload)
+    print(response.text)
