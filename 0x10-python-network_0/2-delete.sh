da2daea1c8ee5a6f34e931e35dccec312de85916
Macksontibande <Macksontibade@gmail.com>
2023-07-15 07:33:23 -0700

    2. cURL Method

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to send a DELETE request to a URL then display the http response message body.
-curl -sX DELETE "$1"
+# Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -s "$1" -X DELETE
